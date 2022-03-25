5dafde17aa5153a2b8b526af693290b7cc31fcb9
gcxfd <i@rmw.link>
2022-03-25 22:22:42 +0800

    🔶

---
+++ web/sh/rm_all_uploaded_file.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+DIR=$(dirname $(realpath "$0"))
+cd $DIR
+set -ex
+
+out=li.txt
+qshell listbucket2 xvc-com -o $out
+qshell batchdelete --force xvc-com -i $out
+rm -rf $out
+rm -rf .id
+rm -rf .uploaded
